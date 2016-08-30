e05d710baeb777057b24bdbaef5bda5e80fe78be
Dario Miroli <s1033855@sms.ed.ac.uk>
2016-08-30 22:35:25 +0100

    Added skeleton of vector class

--- ClassTests/Main.cpp
+++ ClassTests/Main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <vector>
 #include "Particle.h"
+#include "Vec.h"
 
 int main(){
   std::cout << "Starting main" << std::endl;
-  std::vector<Particle> ParticleArray(10);
-  for(int i=0; i < ParticleArray.size(); i++){
-    std::cout <<  ParticleArray[i];
-  }
+
+  Vec v1(1,2,3);
+  Vec v2(4,5,6);
+
+  std::cout << v1 << v2;
+  std::cout << v1.getMag();
+  v1.scalarMultiply(10.0);
+  std::cout << std::endl << v1 << std::endl;
+  std::cout << Vec::add(v1,v2);
+  std::cout << Vec::subtract(v1,v2);
+  std::cout << std::endl << "End of program" << std::endl;
   return 0;
 }
